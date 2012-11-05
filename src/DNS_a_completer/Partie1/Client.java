a8aceb86985c7029ac45dc0f29564dc744ffde90
Laurence de Villers <laurence.de.villers.prog@gmail.com>
2012-11-05 15:12:08 -0500

    MOdele

--- src/DNS_a_completer/Partie1/Client.java
+++ src/DNS_a_completer/Partie1/Client.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:src/DNS_a_completer/Partie1/Client.java
 package Partie1;
 
 public class Client {
@@ -7,3 +8,60 @@ public class Client {
 	}
 
 }
+=======
+package Partie1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+import org.omg.CORBA.portable.InputStream;
+
+public class Client {
+	
+	private Socket clientSocket; 
+	
+	public void openSocket(String host, int serverPort){
+		try {
+			clientSocket = new Socket(host, serverPort);
+		}catch(IOException e ){
+			System.out.println("OpenSocket exeption "+e.getMessage());
+		}
+	}
+	
+	public String readData(){
+		int MAXLENGTH = 256; 
+		byte[] buff = new byte[ MAXLENGTH];
+		String read = null;
+		try{
+			InputStream in = (InputStream) clientSocket.getInputStream();
+			in.read(buff);
+			
+			InputStreamReader is = new InputStreamReader(in);
+			BufferedReader br = new BufferedReader(is);
+			read = br.readLine();
+		}catch(IOException e){
+			System.out.println("Exception readData "+e.getMessage());
+		}
+		return read;
+		
+	}
+	
+	public void closeSocket(){
+		try{
+			clientSocket.close();
+		}catch(IOException e){
+			System.out.println("CloseSocket exception "+e.getMessage());
+		}
+	}
+	
+	
+	public static void main(){
+		
+		
+		
+	}
+
+}
+>>>>>>> MOdele:DNS_a_completer/Partie1/Client.java
--- src/DNS_a_completer/Partie1/Serveur.java
+++ src/DNS_a_completer/Partie1/Serveur.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:src/DNS_a_completer/Partie1/Serveur.java
 package Partie1;
 
 public class Serveur {
@@ -6,3 +7,56 @@ public class Serveur {
 		
 	}
 }
+=======
+package Partie1;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.omg.CORBA.portable.OutputStream;
+
+public class Serveur {
+
+	private Socket connectSocket; 
+	private ServerSocket server;
+	
+	public void connectServeur(int port){
+		
+		try{
+			server = new ServerSocket(port);
+		}catch( IOException e){
+			System.out.println("ServerSocket exception "+e.getMessage());
+		}
+	
+		try {
+			connectSocket = server.accept();
+		}catch(IOException e){
+			System.out.println("Socket exception: "+e.getMessage());
+		}
+	}
+	
+	public void sendMessage(String message){
+		
+		try {
+			OutputStream out = (OutputStream) connectSocket.getOutputStream();
+			out.write( message.getBytes());
+		}catch(IOException e){
+			System.out.println("SendMessage exception"+e.getMessage());
+		}
+	}
+	
+	public void closeSocket(){
+		try{
+			connectSocket.close();
+			server.close();
+		}catch(IOException e){
+			System.out.println("CloseSocket exception"+ e.getMessage());
+		}
+	}
+	
+	public static void main(){
+		
+	}
+}
+>>>>>>> MOdele:DNS_a_completer/Partie1/Serveur.java
